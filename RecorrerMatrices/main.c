ca9846dabb28a9fc5c0c24ae013918aa2eda7ec1
Nomar Mora <nomarmora@gmail.com>
2017-07-09 14:08:50 -0400

    Agregado RecorrerMatrices
    En este codigo se hace el recorrido de matrices, primero para llenarla
    de datos y luego para mostrar dichos datos.
    
    En este caso en la salida se esta haciendo uso de algo de formato.

---
+++ RecorrerMatrices/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int f = 3, c = 3;
+    int Matriz[f][c];
+    int i,j;
+    for (i = 0; i < f; i++){
+        for (j = 0; j < f; j++){
+            printf("Introduzca el valor [%d:%d] ", i+1, j+1);
+            scanf("%d", &Matriz[i][j]);
+        }
+    }
+    printf("Mostremos los datos ahora....\n");
+    
+    for (i = 0; i < f; i++){
+        for (j = 0; j < f; j++){
+            scanf("%5d", Matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Press enter to continue ...");
+    system("pause");
+    return 0;
+
+}
